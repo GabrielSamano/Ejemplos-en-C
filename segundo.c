8e7a7c98e0bfa1e8f23a0999bf2cae4668c0b67f
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-09-19 10:51:22 -0500

    Creación de variables

---
+++ segundo.c
@@ -0,0 +1,15 @@
+// Creando variables
+
+int main() {
+    int MyNum = 5;  // Entero
+    float MyFloat = 5.64;  //Flotante
+    char Mychar = "B";  // Caracter
+
+    // IMprimiendo las variables con sus especificadores
+
+    printf("%i\n", MyNum);
+    printf("%f\n", MyFloat);
+    printf("%c\n", Mychar);
+    return 0;
+
+}
