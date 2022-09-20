b69950a25e890d9932c392c38df73a0ed67a069c
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-09-20 09:33:44 -0500

    Operadores de Comparación

---
+++ cuatro.c
@@ -0,0 +1,20 @@
+
+// Operadores de Comparacion 
+
+int main() {
+    int x = 5;
+    int y = 7;
+
+    // Tamaño de los operadores
+    int MyInt;
+    float MyFloat;
+    double MyDouble;
+    char MyChar;
+
+    printf("%d\n", x > y);
+    printf("%lu\n", MyInt);
+    printf("%lu\n", MyFloat);
+    printf("%lu\n", MyDouble);
+    printf("%lu\n", MyChar);
+    return 0;
+}
