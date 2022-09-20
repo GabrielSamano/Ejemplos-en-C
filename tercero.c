3515545aee89079283748fc6faf94225939bee61
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-09-20 09:06:06 -0500

    Constantes en C

---
+++ tercero.c
@@ -0,0 +1,16 @@
+
+// Utilizamos const para crear variables con valores que no cambiarán
+// Operadores en C  
+
+int main() {
+    const int BirthdayYear = 2000;
+    // Sumas de variables 
+    float sum1 = 20.5 + 13;
+    float sum2 = sum1 + 4.7;
+
+    printf("%i\n", BirthdayYear);
+    printf("%f\n", sum1);
+    printf("%f\n", sum2);
+
+    return 0;
+}
