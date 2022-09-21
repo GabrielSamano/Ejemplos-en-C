ebb90bb502420ea0f3f651f2218dbca2199f4e34
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-09-21 09:17:18 -0500

    Bucle for

---
+++ Octavo.c
@@ -0,0 +1,13 @@
+
+// Bucle for
+
+int main() {
+    int i;
+    for ( i = 0; i <= 20; i = i +2)
+    {
+        printf("%d\n", i);
+    }
+    return 0;
+}
+
+// La salida sera "0,2,4,6,8,10,12,14,16,18,20"
