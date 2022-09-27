2593b2bbb550f1fe4c98b6ea585c52cebe1bc9e9
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-09-27 10:23:22 -0500

    Entrada de usuario

---
+++ once.c
@@ -0,0 +1,15 @@
+
+// Entrada de Usuario en C
+// Utilizando la funcion scanf()
+
+int main() {
+    char FirstName[30];
+
+    printf("Ingresa tu primer nombre: \n");
+
+    scanf("%s", FirstName);
+
+    printf("Hola %s", FirstName);
+
+    return 0;
+    }
