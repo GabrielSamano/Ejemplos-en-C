140966f99899b542c3cf2a4ec39cdfe367c5a095
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-09-27 10:23:56 -0500

    Funciones

---
+++ Doce.c
@@ -0,0 +1,16 @@
+
+// Creando funciones
+
+void Myfunction() {
+    printf("Primera funcion en C");
+
+
+}
+
+int main() {
+    Myfunction();
+    return 0;
+}
+
+
+// LLamara a la funcion e imprimira "Primera funcion en C"
