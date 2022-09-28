625dc6275e5241abf456af1e9e77ea2856a70c30
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-09-28 13:16:54 -0500

    Buena Practica

--- catorce.c
+++ catorce.c
@@ -15,3 +15,18 @@ int main() {
 }
 
 // La silda sera la suma de 10 y 4 = "14"
+
+// Se considera buena practica escribir el codigo de la siguiente manera
+
+// int Myfunction(int, int);
+//
+//int main(){
+//    int Result = Myfunction(10,4); 
+//  pritnf("Result  is : %d", Result);
+//  return 0;
+//  }
+//
+//  int Myfunction(int x, int y){
+//      return x + y;
+//  }
+//
