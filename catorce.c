2bb30392ca1c5e3f8141318697600ec76e531230
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-09-28 13:09:17 -0500

    Declaración de función

---
+++ catorce.c
@@ -0,0 +1,17 @@
+
+// Declaracion y definicion de las funciones 
+
+int Myfunction(int x, int y) {
+    return x + y;
+}
+
+int main() {
+    int result = Myfunction(10,4);
+
+    printf("Result is : %d", result);
+
+    return 0;
+
+}
+
+// La silda sera la suma de 10 y 4 = "14"
