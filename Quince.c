bd7f1a27bb7202db55d824a530d66a94fda65e17
GabrielSamano <88937668+GabrielSamano@users.noreply.github.com>
2022-09-28 13:33:44 -0500

    Recursión

---
+++ Quince.c
@@ -0,0 +1,21 @@
+
+// Recursion en C
+
+int sum(int k);
+
+int main () {
+    int result = sum(20);
+    printf("%d", result);
+    return 0;
+}
+
+
+int sum(int k) {
+    if (k > 0) {
+        return k + sum(k -1);
+    }else {
+        return 0;
+    }
+}
+
+// La salida sera "210"
